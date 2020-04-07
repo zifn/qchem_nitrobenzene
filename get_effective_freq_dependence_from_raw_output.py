8ce7988096b9d64f6a9f9dec670504f8dc748704
zifn <zifn14@gmail.com>
2020-04-07 01:14:11 -0700

    fixed minor typos and import errors

--- get_effective_freq_dependence_from_raw_output.py
+++ get_effective_freq_dependence_from_raw_output.py
@@ -3,6 +3,7 @@ import os
 
 from scipy import constants as consts
 import pandas as pd
+import numpy as np
 
 import main_conversion
 
@@ -29,13 +30,15 @@ def main(dir_path, output_file_name, should_return=False):
     output_data = []
     for file in os.listdir(folder_location):
         if is_dalton_output_file(file):
-            print('found file  = {}'.format(file))
-            freq = find_frequency_used(file)
+            #print('found file  = {}'.format(file))
+            freq = find_parameter_used(file)
+            freq_perm = find_parameter_used(file, 'perm-')
             file_output = main_conversion.main_conversion(os.path.join(folder_location, file), True)
             if file_output:
                 chi3_eff_sym, chi3_eff_expr, chi3_eff_value, gamma_eff_value, gamma_rot_ave, chi3_rot_ave, chi3_sym, lambda_out, lambda_1, lambda_2, lambda_3, warning_flag = file_output
                 output_data.append({'freq (Hartree)': freq,
                                     'freq (eV)': abs(convert_hartree_freq_to_eV(freq)),
+                                    'freq_permutation': freq_perm,
                                     'gamma effective': gamma_eff_value,
                                     'chi3 effective': chi3_eff_value,
                                     'warning_flag': warning_flag})
